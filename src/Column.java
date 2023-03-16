34ef6543f69412ac8dd71a195e60286a4703ba46
aly raafat <alewaraafat@gmail.com>
2023-03-16 18:13:10 +0200

    refactoring code

---
+++ src/Column.java
@@ -0,0 +1,44 @@
+public class Column{
+    private String tableName;
+    private String columnName;
+    private String columnType;
+    private boolean isClusteringKey;
+    private Object min;
+    private Object max;
+
+    public Column(String tableName , String columnName , String columnType , boolean isClusteringKey , Object min, Object max){
+        this.tableName = tableName;
+        this.columnName = columnName;
+        this.columnType = columnType;
+        this.isClusteringKey = isClusteringKey;
+        this.min = min;
+        this.max = max;
+    }
+    public String toString() {
+        return tableName + "," + columnName + "," + columnType + "," + (isClusteringKey ? "True" : "False") + "," + min.toString()+ ","+max.toString();
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public String getColumnType() {
+        return columnType;
+    }
+
+    public boolean isClusteringKey() {
+        return isClusteringKey;
+    }
+
+    public Object getMin() {
+        return min;
+    }
+
+    public Object getMax() {
+        return max;
+    }
+}
--- src/Metadata.java
+++ src/Metadata.java
@@ -56,7 +56,6 @@ public class Metadata {
             fw= new FileWriter(metafile,true);
             StringBuilder sb= new StringBuilder();
 
-
             Set<Entry<String, String>> entrySet = htblColNameType.entrySet();
 
             for (Entry<String, String> entry : entrySet) {
@@ -96,7 +95,8 @@ public class Metadata {
     public int getTupleSize(String tableName){
         int size = 0;
         for (int i = 0 ; i < columnsOfMetaData.size() ; i++){
-            if(columnsOfMetaData.get(i).getTableName().equals(tableName)){
+            String currentTablename = columnsOfMetaData.get(i).getTableName();
+            if(currentTablename.equals(tableName)){
                 size++;
             }
         }
@@ -126,47 +126,4 @@ public class Metadata {
 
 }
 
-class Column{
-    private String tableName;
-    private String columnName;
-    private String columnType;
-    private boolean isClusteringKey;
-    private Object min;
-    private Object max;
 
-    public Column(String tableName , String columnName , String columnType , boolean isClusteringKey , Object min, Object max){
-        this.tableName = tableName;
-        this.columnName = columnName;
-        this.columnType = columnType;
-        this.isClusteringKey = isClusteringKey;
-        this.min = min;
-        this.max = max;
-    }
-    public String toString() {
-        return tableName + "," + columnName + "," + columnType + "," + (isClusteringKey ? "True" : "False") + "," + min.toString()+ ","+max.toString();
-    }
-
-    public String getTableName() {
-        return tableName;
-    }
-
-    public String getColumnName() {
-        return columnName;
-    }
-
-    public String getColumnType() {
-        return columnType;
-    }
-
-    public boolean isClusteringKey() {
-        return isClusteringKey;
-    }
-
-    public Object getMin() {
-        return min;
-    }
-
-    public Object getMax() {
-        return max;
-    }
-}
