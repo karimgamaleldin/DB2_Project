ab8e64f5e0407fbaa38aea70365375cd5393a250
aly raafat <alewaraafat@gmail.com>
2023-04-23 01:48:22 +0200

    adjust some things in m2

--- src/index/Node.java
+++ src/index/Node.java
@@ -1,18 +1,28 @@
 package index;
 
+import java.util.Hashtable;
 import java.util.Vector;
 
 public class Node {
     private Vector<Node> childPoints;
-    private double x, y, z;
+    private Object x, y, z;
+    boolean isLeaf;
     private int maxEntries;
 
-    public Node(double x, double y, double z, int maxEntries) {
+    public Node(Object x, Object y, Object z, boolean isLeaf, int maxEntries) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.childPoints = new Vector<Node>();
+        this.isLeaf = isLeaf;
         this.maxEntries = maxEntries;
+    }
+    public void insertIntoNode(Object x, Object y, Object z){
+        if(this.childPoints.size()<maxEntries){
+            //insert in right sorted place in childPoints
+        }else {
+            //divide x, y, z into halves and do it recursively according
+        }
     }
     public Vector<Node> getChildPoints() {
         return childPoints;
@@ -22,7 +32,7 @@ public class Node {
         this.childPoints = childPoints;
     }
 
-    public double getX() {
+    public Object getX() {
         return x;
     }
 
@@ -30,7 +40,7 @@ public class Node {
         this.x = x;
     }
 
-    public double getY() {
+    public Object getY() {
         return y;
     }
 
@@ -38,7 +48,7 @@ public class Node {
         this.y = y;
     }
 
-    public double getZ() {
+    public Object getZ() {
         return z;
     }
 
@@ -53,5 +63,23 @@ public class Node {
     public void setMaxEntries(int maxEntries) {
         this.maxEntries = maxEntries;
     }
+    public void setX(Object x) {
+        this.x = x;
+    }
 
+    public void setY(Object y) {
+        this.y = y;
+    }
+
+    public void setZ(Object z) {
+        this.z = z;
+    }
+
+    public boolean isLeaf() {
+        return isLeaf;
+    }
+
+    public void setLeaf(boolean leaf) {
+        isLeaf = leaf;
+    }
 }
--- src/index/Octree.java
+++ src/index/Octree.java
@@ -1,5 +1,62 @@
 package index;
 
+
+import java.util.Hashtable;
+
 public class Octree {
-    Node root;
+    private Node root;
+    private String col1, col2, col3;
+    private int maxEntries;
+    public Octree(String col1, String col2, String col3, int maxEntries){
+        root = new Node(null,null,null,true,maxEntries);;
+        this.col1 = col1;
+        this.col2 = col2;
+        this.col3 = col3;
+        this.maxEntries = maxEntries;
+    }
+    public void insertIntoOctree(String strTableName, Hashtable<String,Object> htblColNameValue){
+        Object valOfCol1 = htblColNameValue.get(col1);
+        Object valOfCol2 = htblColNameValue.get(col2);
+        Object valOfCol3 = htblColNameValue.get(col3);
+        root.insertIntoNode(valOfCol1,valOfCol2,valOfCol3);
+    }
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public String getCol1() {
+        return col1;
+    }
+
+    public void setCol1(String col1) {
+        this.col1 = col1;
+    }
+
+    public String getCol2() {
+        return col2;
+    }
+
+    public void setCol2(String col2) {
+        this.col2 = col2;
+    }
+
+    public String getCol3() {
+        return col3;
+    }
+
+    public void setCol3(String col3) {
+        this.col3 = col3;
+    }
+
+    public int getMaxEntries() {
+        return maxEntries;
+    }
+
+    public void setMaxEntries(int maxEntries) {
+        this.maxEntries = maxEntries;
+    }
 }
